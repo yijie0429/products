262377079bd91b48f655a9fdb5b31975463baddb
cyj0429 <amy902001@gmail.com>
2023-03-13 00:52:14 +0800

    add for loop

--- products.py
+++ products.py
@@ -5,9 +5,10 @@ while True:
         break
     price = input('請輸入商品價格: ')
     products.append([name, price])
-print(products)
-
+print(products)                      #印出橫的
 print(products[0][0])
 
+for p in products:                   #印出直的:每讀取一次，印一次
+    print(p[0], '的價格是', p[1])                      
 
     
