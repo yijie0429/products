3408af6490930a784baf1e5ea09af0fe3e7d9357
cyj0429 <amy902001@gmail.com>
2023-03-15 16:22:45 +0800

    add read file

--- products.py
+++ products.py
@@ -1,5 +1,14 @@
+#讀取檔案
 products = []
+with open ('products.csv', 'r',encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+print(products)
 
+#讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -8,13 +17,14 @@ while True:
     price = int(price)
     products.append([name, price])
 print(products)                                #印出橫的
-print(products[0][0])
 
+#印出所有購買紀錄
 for p in products:                             #印出直的:每讀取一次，印一次
     print(p[0], '的價格是', p[1])                      
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:           #打開一個檔案來寫入 #csv可用excel打開
-    f.write('商品,價格,\n')
+    f.write('商品,價格\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')      #四個都是字串，用字串合併
 
